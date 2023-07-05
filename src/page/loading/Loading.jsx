5ca32fcfd5076b86ecb58001e5b088fde184c543
lucky2588 <duwcsthang2001@gmail.com>
2023-07-06 00:14:21 +0700

    Create Loading.jsx

---
+++ src/page/loading/Loading.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+
+function Loading() {
+    return (
+        <>
+            {/* <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Growing Color spinners</h5>
+            
+            <div className="spinner-grow text-primary" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-secondary" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-success" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-danger" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-warning" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-info" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-light" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-dark" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Sizes</h5>
+            <p>Add <code>.spinner-border-sm</code> and <code>.spinner-grow-sm</code> to make a smaller spinner that can quickly be used within other components. Or, use custom CSS or inline styles to change the dimensions as needed.</p>
+          
+            <div className="spinner-border spinner-border-sm" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-border" style={{width: '30px', height: '30px'}} role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-border" style={{width: '40px', height: '40px'}} role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-border" style={{width: '50px', height: '50px'}} role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow spinner-grow-sm" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow" style={{width: '30px', height: '30px'}} role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow" style={{width: '40px', height: '40px'}} role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow" style={{width: '50px', height: '50px'}} role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          
+          </div>
+        </div> */}
+ <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+ 
+            <div class="">
+                <div class="">
+                    <h5 class="card-title text-center">Vui lòng chờ trong giây lát </h5>
+                    <p class="text-center"> Chúng tôi đang lấy dữ liệu từ kho lưu trữ dữ liệu của chúng tôi ,.... mong bạn thông cảm !!  </p>
+
+
+                    <div class="d-flex justify-content-center">
+                        <div class="spinner-border" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br /> <br />
+            <br />
+            <br />
+
+        </>
+    )
+}
+
+export default Loading
