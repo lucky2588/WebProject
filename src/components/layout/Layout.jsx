55f2ce894610b709f79e374becc1f49aa501f505
lucky2588 <duwcsthang2001@gmail.com>
2023-07-06 00:12:42 +0700

    Create Layout.jsx

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../header/Header'
+import { Outlet } from 'react-router'
+import Footer from '../footer/Footer'
+
+function Layout() {
+  return (
+    <>
+          <Header />
+
+          <Outlet />
+
+          <Footer />
+    </>
+  )
+}
+
+export default Layout
